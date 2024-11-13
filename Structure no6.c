515c82576608082d78034c831e76ae68d5acc979
kugukugu <n-a-v-e-r_is_a_firm@naver.com>
2024-11-13 21:04:52 +0900

    Update Structure no6.c

--- Structure no6.c
+++ Structure no6.c
@@ -3,13 +3,35 @@
 #include <time.h>
 #include <string.h>
 
+enum job
+{
+    police=1, ceo, doctor, teacher
+};
+
+char* print_job(enum job k)
+{
+    switch(k)
+    {
+        case police :
+            return "POLICE";
+        case ceo :
+            return "CEO";
+        case doctor :
+            return "doctor";
+        case teacher :
+            return "teacher";
+        default:
+            return NULL;
+    }
+}
+
 struct member
 {
     char id[31];
     char *name;
     char birth[9];
     char phone_number[14];
-    char job[10];
+    enum job p;
     int age;
     char mf;
 };
@@ -23,21 +45,21 @@ int valid(char *p)
     {
         return 0;
     }
-    
+
     if(p[3] != '-' || p[8] != '-')
     {
         return 0;
     }
-    
+
     if(p[0] != '0'|| p[1] != '1' || p[2] != '0')
     {
         return 0;
     }
-    
+
     for(i=0; i<4; i++)
     {
          for(j=48; j<=57; j++)
-         {   
+         {
              if((int)p[4+i] == j)
              {
                  k+=1;
@@ -57,6 +79,7 @@ int valid(char *p)
 
     return 1;
 }
+
 void id_random_generate(struct member* p)
 {
     int i , k;
@@ -92,6 +115,7 @@ void input_name (char **p)
 
 int main(void)
 {
+    int u;
 
     srand(time(NULL));
     struct member p;
@@ -100,8 +124,27 @@ int main(void)
 
     printf("주민등록번호 - 뒤 1자리 입력하시오.\n");
     scanf("%s", p.birth);
-    printf("직업을 입력하시오.\n");
-    scanf("%s", p.job);
+    printf("직업을 선택하시오.\n");
+
+    for( u = police; u<= teacher; u++)
+    {
+        printf("%d. %s\n", u, print_job(u));
+    }
+
+    do
+    {
+        scanf("%d", &p.p);
+
+        if(print_job(p.p) != NULL )
+        {
+            break;
+        }
+
+        printf("잘못된 입력\n");
+
+    }while(1 );
+
+
 
     int i;
     char temp[5];
@@ -145,12 +188,14 @@ int main(void)
 
     id_random_generate(&p);
 
+    printf("\n");
+
     printf("%s\n", p.id);
     printf("%s\n", p.name);
     printf("%s\n", p.birth);
     printf("%s\n", p.phone_number);
     printf("%d\n", p.age);
-    printf("%s\n", p.job);
+    printf("%s\n", print_job(p.p));
     printf("%c\n", p.mf);
 
     free(p.name);
