3e0c0bd34cb5ffb388da3dd0bf483790f61936a3
kugukugu <n-a-v-e-r_is_a_firm@naver.com>
2024-11-30 22:49:08 +0900

    Update Structure no7.c

--- Structure no7.c
+++ Structure no7.c
@@ -9,7 +9,7 @@ struct member
     int age;
 };
 
-int compare( struct member *a, struct member *b)
+int compare(struct member* a, struct member* b)
 {
     return strcmp(a->name, b->name);
 
@@ -20,48 +20,51 @@ int main(void)
 
     srand(time(NULL));
 
-   struct member s[10];
+    struct member s[10];
 
-   int i,j,N;
+    int i, j, N;
 
-   for(i=0; i<10; i++)
-   {
-       for(j=0; j<rand() % 11+2; j++)
-       {
-           s[i].name[j] = rand() % 26 + 'a';
-       }
-       s[i].name[j] = '\0';
-       s[i].age= rand() % 100 + 1;
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < rand() % 11 + 2; j++)
+        {
+            s[i].name[j] = rand() % 26 + 'a';
+        }
+        s[i].name[j] = '\0';
+        s[i].age = rand() % 100 + 1;
 
-    printf("%s %d\n", s[i].name, s[i].age);
-   }
-
-   printf("\n");
-
-   qsort(s, 10, sizeof(struct member), compare);
-
-   for(i=0; i<10; i++)
-   {
         printf("%s %d\n", s[i].name, s[i].age);
-   }
+    }
 
-   printf("나이대를 입력하시오. (10 단위)");
-   scanf("%d", &N);
+    printf("\n");
 
-   printf("%d대 나이: ", N);
+    qsort(s, 10, sizeof(struct member), compare);
 
-   for(i=0; i<10; i++)
-   {
-       if( N == 0 )
-       {
-            printf("%d ",s[i].age);
-       }
-       else if( s[i].age/N == 1 )
-       {
-           printf("%d ",s[i].age);
-       }
+    for (i = 0; i < 10; i++)
+    {
+        printf("%s %d\n", s[i].name, s[i].age);
+    }
 
-   }
+    printf("나이대를 입력하시오. (10 단위)");
+    scanf("%d", &N);
 
-	return 0;
+    printf("%d대 나이: ", N);
+
+    j = 0;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (s[i].age - N  >= 0 &&  s[i].age - N  < 10)
+        {
+            printf("%d ", s[i].age);
+            j++;
+        }
+    }
+
+    if (j == 0)
+    {
+        printf("존재하지 않습니다.\n");
+    }
+
+    return 0;
 }
