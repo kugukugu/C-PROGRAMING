72d26cd51939d7b4d2e85badc5cc8429f3f8114e
kugukugu <n-a-v-e-r_is_a_firm@naver.com>
2024-11-30 09:42:07 +0900

    Update Pointer no8.c

--- Pointer no8.c
+++ Pointer no8.c
@@ -7,36 +7,40 @@ int main()
 {
     // 다항식의 정보를 입력받아 저장한다. (모두 자연수로 가정)
       
-    int N, coe[100] = { 0 }, degree[100] = { 0 }, x, sum=0;
+    int N, x, sum = 0;
     printf("항의 개수를 입력하시오.");                                        // -항의 개수 N
     scanf("%d", &N);
     int i,j;
+
+    int* p;
+    int* q;
+    int* r;
+
+    p = (int*)malloc(sizeof(int) * N);
+    q = (int*)malloc(sizeof(int) * N);
+    r = (int*)malloc(sizeof(int) * N);
 
     printf("각 항의 계수를 입력하시오.\n");
     for (i = 0; i < N; i++)                                                  // - 각 항의 계수 coeffcients
     {
         printf("%d번 째 항",i+1);
-        scanf("%d", &coe[i]);
+        scanf("%d", &q[i]);
     }
 
     printf("각 항의 차수를 입력하시오.\n");
     for (i = 0; i < N; i++)
     {
         printf("%d번 째 항", i + 1);                                        //  - 각항의 차수 degree
-        scanf("%d", &degree[i]);
+        scanf("%d", &r[i]);
     }
-
-    int* p;
     
     printf("미지수 x를 입력하시오.");                                   //   미지수 X를 입력받아 다항식의 값을 계산하여 출력한다.
     scanf("%d", &x);
 
-    p = (int*)malloc(sizeof(int) * N);
-
     for (i = 0; i < N; i++)
     {
-        p[i] = coe[i];
-        for (j = 0; j < degree[i]; j++)
+        p[i] = q[i];
+        for (j = 0; j < r[i]; j++)
         {
             p[i] = p[i] * x;
         }
@@ -46,6 +50,8 @@ int main()
     printf("sum = %d", sum);
 
     free(p);
+    free(q);
+    free(r);
 
 	return 0;
 }
