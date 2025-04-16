c97c457c20e5427bf68a63be99cdadda1d648542
kugukugu <n-a-v-e-r_is_a_firm@naver.com>
2025-04-16 19:21:27 +0900

    Create 2020년 2학기 자료구조 중간고사 no3.c

---
+++ 2020년 2학기 자료구조 중간고사 no3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+void deleteChars(char *str, char *ch) 
+{
+	int length =strlen(ch);
+	int i,j;
+	char *p=str, *p1= str, *p2=str;
+	char str1[100]={0};
+
+	for(i=0; p[i]!='\0'; i++) //hello world
+	{
+		for(j=0; j<length; j++)
+		{
+			if(p[i]==ch[j])
+			{
+				p1=&p[i];
+				strncat(str1,p2,p1-p2);
+				p1+=1;
+				p2=p1;
+			}
+		}
+	}
+	strcat(str1,p1);
+	strcpy(str,str1);
+}
+
+int main()
+{
+	char str[128];
+
+	strcpy(str, "hello world");
+	deleteChars(str, "lo");
+	printf("%s\n", str);
+
+	strcpy (str, "Hello world");
+	deleteChars(str,"H");
+	printf("%s\n", str);
+
+	return 0;
+}
